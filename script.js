2e5f305d0005a659f2a411beeaab5e7688796e1b
hihi01000 <125710354+hihi01000@users.noreply.github.com>
2023-03-28 12:17:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const title = document.getElementById("title");
-
+const image = document.getElementById("image");
 title.addEventListener("mouseover", () => {
     title.style.fontSize = "3rem";
 });
 
 title.addEventListener("mouseout", () => {
     title.style.fontSize = "2rem";
+});
+image.addEventListener("mouseover", () => {
+    image.style.transform = "scale(1.5)";
+});
+
+image.addEventListener("mouseout", () => {
+    image.style.transform = "scale(1)";
 });
